b0694aecea117445d9aaa96c5b9786b28271829f
marcospiau <marcospiau123@gmail.com>
2023-06-20 23:16:44 -0300

    improve argparse

--- upload_folder_to_hf_hub.py
+++ upload_folder_to_hf_hub.py
@@ -1,14 +1,27 @@
 import argparse
 from huggingface_hub import HfApi
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--local_folder', type=str, default='final_train_A100_v1')
-parser.add_argument('--repo_id', type=str, default='colbert-team/colbert-vqa')
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--local_folder',
+                    type=str,
+                    default=None,
+                    help='Path to local folder to upload to HuggingFace Hub')
+parser.add_argument('--repo_id',
+                    type=str,
+                    default=None,
+                    help='HuggingFace Hub repo id to upload to')
+parser.add_argument('--path_in_repo',
+                    type=str,
+                    default=None,
+                    help='Path to upload to in HuggingFace Hub repo')
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    hf_api = HfApi()    
+    hf_api = HfApi()
+    hf_api.create_repo(repo_id=args.repo_id, exist_ok=True)
     hf_api.upload_folder(
         folder_path=args.local_folder,
-        repo_id=args.repo_id)
+        repo_id=args.repo_id,
+        path_in_repo=args.path_in_repo,
+    )
